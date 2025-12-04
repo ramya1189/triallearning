e406b2b777254b7aa3d79d63d66eae700332265a
ramya1189 <ramya60100@gmail.com>
2025-12-04 12:41:09 +1100

    saved the code in variables

---
+++ Traineeship/GIT/Ramya's Typescript/src/variables.ts
@@ -0,0 +1,28 @@
+//Variables are used to store values like numbers , 
+// text so that it can be used in the program later.
+// think of variables like a container, that holds something
+
+// there are three ways to declare a variable
+
+//let - can change late
+
+let age = 35;
+console.log("Age="+age);
+
+//const - it cannot be changed later
+
+const Name = "Ramya";
+console.log("Name:" + Name);
+
+//var - can be changed later
+
+var city = "Canberra";
+console.log("City = " + city);
+
+let Name1 = "Ramya";
+let Age1 = 36;
+let city1 = "Canberra";
+
+console.log("Name = "+ Name1);
+console.log("Age = " + Age1);
+console.log("City = " + city1);
